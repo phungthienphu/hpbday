db787daba0c39efa35a6ac2cdba337d5b0e26eca
PhuNetko <phu.phung@netko-solution.com>
2025-11-25 15:58:50 +0700

    hot fix client api

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: import.meta.env.VITE_API_URL_LOCAL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default client;
